b013f2b4eb02aa37aa87e53c9e77f9f9d40e745f
John Hodgson <johnhodgson@johnsmacbook.lan>
2019-10-27 15:11:29 +0000

    Removed files

--- src/containers/AlbumPage/constants.js
+++
@@ -1,4 +0,0 @@
-export const GET_ALBUM_BY_ID = 'bands-app/AlbumPage/GET_ALBUM_BY_ID';
-export const SET_ALBUM_ID = 'bands-app/AlbumPage/SET_ALBUM_ID';
-export const CLEAR_ALBUM_PAGE = 'bands-app/AlbumPage/CLEAR_ALBUM_PAGE';
-export const SET_LOADING = 'bands-app/AlbumPage/SET_LOADING';
